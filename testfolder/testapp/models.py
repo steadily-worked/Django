9842ad2850da7fd1df68be2b8e8abeeb24d2560d
sangminpark <qpwpep5429@gmail.com>
2020-07-03 15:17:46 +0900

    6/30일분 까지

---
+++ testfolder/testapp/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    "id = int => 각 게시물에 고유한 번호를 배정. 학번 같은 느낌임"
+    title = models.CharField(max_length = 20)
+    #title이라는 열을 만들고 models -> Char(제한적 길이를 가진 문자) -> max_length
+    body = models.TextField()    #body라는 열을 만들고 Text(제한이 없는 문자열)
+    pub_date = models.DateTimeField('published time')
+    #pub_date라는 열을 만들고, 그 형식을 날짜/시간의 형태로 넣겠다.
+
+    def __str__(self):
+        return self.title
+
+    #"python manage.py makemigrations -> 파이썬으로 작성된 것을 DB 언어로 바꿔준다."
+    #"python manage.py migrate -> DB에 적용"
+
+
+    #models.py에서
+    #class 테이블 이름(models.Model):
+        #열 이름 = models.[데이터 열]
+        #def __str__(self):
+            #return self.title
+    #"python manage.py makemigrations -> 파이썬으로 작성된 것을 DB 언어로 바꿔준다."
+    #"python manage.py migrate -> DB에 적용"
+    #그 이후에 /admin -> login 하라고 함. python manage.py createsuperuser
+    #그 이후 admin.py에서, admin.site.register(Post)
+    #html에 띄워야 되니까 .. views.home에 가서 result = Post.objects.all() -> 정의한 DB 테이블을 가져와서 모든 객체를 가져왔음.
+    #이걸 변수로 담아서 home.html에 줬고, html에서 for문을 이용해서 for i in 테이블: i.title 이런 식으로 넣어 줬음.
+    #views에서 detail 함수를 정의해준 후 detail.html을 만들어 줬음
+    #'detail/<int:변수>'.views.detail().....
+    #detail(request):
+    #detail(request, 변수=번수)
+    #그 이후에 views.py로 가서 detail 함수를 바꿔줌. (detail(request, "변수") 이런식으로 변수를 추가해줘서 2개를 받게 함)
+    #그 이후, 변수 추출을 .. view.py에 detail 함수에서 post.objects.get(id="변수")
+    #return render(request, 'detail.html', {'content' : content}) 이런식으로 "detail.html" 을 추가해줌.
+    # "{% url 'detail' post.id %}"을 home.html에다가 적어주고 ..
+    #그 이후 detail.html에서 {{result.title}}, {{result.pub_date}} 이런 식으로 받아준 것.
+    #마지막으로, home.html로 돌아가는 건, <a href="{% url 'home' %}">home으로 가기</a> 이런 식으로 써주면 된다. 다른 값을 받아 올 필요가 없기 때문.
---
+++ testfolder/testapp/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect
+from .models import Post
+from django.utils import timezone
+
+# Create your views here.
+def home(request):
+    result = Post.objects.all()
+        #Post.objects() : 표 전체
+        #Post.objects.all() : 표 내에 행 전체 ...
+        #.all => 게시물의 행들을 list로 가져와 준다. => 메소드(객체를 다루는 함수들)
+        #.filter(), .order_by() => 다 메소드임.
+        #QuerySet => 쿼리(요청) => set. DB가 준 것을 Set하는 과정.
+    print(result)
+    return render(request, 'home.html', {"post_list" : result})
+
+def detail(request, num):
+ #값이 리스트로 넘어옴. => 여러개를 추출해줄 수 있다.
+    content = Post.objects.get(id=num) #행 id가 num인 것을 1개 준다.
+    return render(request, 'detail.html', {'content' : content})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    post = Post()
+    post.title = request.GET['title']
+    post.body = request.GET['body']
+    post.pub_date = timezone.datetime.now()
+    post.save()
+    return redirect('/detail/'+str(post.id))
+
+def update(request, num):
+    post = Post.objects.get(id=num)
+    if request.method == "POST":
+        #수정된 글을 저장해주겠다
+        post.title = request.POST['title']
+        post.body = request.POST['body']
+        post.save()
+        return redirect('/detail/' + str(post.id))
+    #수정된 글을 보내주겠다.
+    return render(request, 'update.html', {"result" : post})
+    pass
+
+def delete(request, num):
+    post = Post.objects.get(id = num)
+    post.delete()
+    return redirect('/')
